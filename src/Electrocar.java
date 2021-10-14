09e6e994f1bb390f60d931d8ff6b4508617a03d8
BERSHOV-AO <bershov.aleksey@bk.ru>
2021-10-14 12:45:48 +0300

    Initial commit

---
+++ src/Electrocar.java
@@ -0,0 +1,35 @@
+public class Electrocar {
+    private int id;
+
+    private class Motor{
+        public void startMotor(){
+            System.out.println("Motor " + id+ " is starting....");
+        }
+    }
+   // статический вложенный класс
+    public static class Battery{
+        public void charge(){
+            System.out.println(" Battery is charging....");
+        }
+    }
+
+    public Electrocar(int id){
+        this.id = id;
+    }
+
+    public void start(){
+
+        Motor motor = new Motor();
+        motor.startMotor();
+
+        final int x = 1;
+
+        class SomeClass {
+            public void someMethod(){
+
+            }
+        }
+
+        System.out.println("Electrocar " + id + " is starting...");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Electrocar electrocar = new Electrocar(10);
+        electrocar.start();
+        Electrocar.Battery battery = new Electrocar.Battery();
+    }
+}
